6735930feb89b27ce7a621974f9212255de58d71
Utilisateur <user@debian.arcahe.ovh>
2020-02-26 12:21:59 +0100

    ajout fonction separees employe et affichage

---
+++ src/employe/AfficheEmploye.java
@@ -0,0 +1,15 @@
+package employe;
+
+public class AfficheEmploye {
+
+	public AfficheEmploye()
+	{
+		;
+	}
+	
+	public void SortieEmploye(Employe e)
+	{
+		System.out.println(e.getAll());
+	}
+}
+
---
+++ src/employe/Employe.java
@@ -0,0 +1,25 @@
+package employe;
+
+public class Employe {
+
+	private final String nom;
+	private final String adresse;
+	
+	public Employe(String nom, String adresse)
+	{
+		this.nom = nom;
+		this.adresse = adresse;
+
+	}
+	
+	public String getAll()
+	{
+		return nom + " " + adresse + " ";
+	}
+	
+	public int CalculSalaire()
+	{
+		System.out.println("salaire : " + 500);
+		return 0;
+	}
+}
